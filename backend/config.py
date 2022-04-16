e243353969113e28715c2414f076d4383fad6ec5
santhosh <august.vs.sant@gmail.com>
2022-04-16 11:25:47 +0530

    create config template

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 # tiger_graph credentials
-TG_HOSTNAME: str = "https://united-heaven-test.i.tgcloud.io/"
-TG_USERNAME: str = "tigergraph"
-TG_PASSWORD: str = "tigergraph"
-TG_GRAPH_NAME: str = "UnitedHeavenG2"
-TG_SECRET: str = "pldbb9lv29gt3fj5ksn63q22tq0ek81p"
-TG_TOKEN: str = ""
+TG_HOSTNAME: str = "<tigergraph hostname>"
+TG_USERNAME: str = "<tigergraph username>"
+TG_PASSWORD: str = "<tigergraph password>"
+TG_GRAPH_NAME: str = "<tigergraph graphname>"
+TG_SECRET: str = "<tigergraph secret>"
+TG_TOKEN: str = ""  # TG_TOKEN will be updated automatically
